b4fa7f4758ed4e87b9c59302e58bf920a1ae5c01
Imane47250 <elaceriimane@gmail.com>
2023-05-30 23:24:54 -0700

    specifiersformat

---
+++ speci.c
@@ -0,0 +1,82 @@
+#include "main.h"
+/**
+ * pchar - prints character
+ * All rights reserved or owned by Imane47250 and Fatimazahralachkar
+ *
+ * @list: listing of arg
+ * Return: return characters printed
+ */
+int pchar(va_list list)
+{
+	charwriting(va_arg(list, int));
+	return (1);
+}
+
+/**
+ * pstring - Prints a string
+ * All rights reserved or owned by Imane47250 and Fatimazahralachkar
+ *
+ * @list: listing of arg
+ * Return: return characters printed
+ */
+int pstring(va_list list)
+{
+	int i;
+	char *str;
+
+	str = va_arg(list, char *);
+	if (str == NULL)
+		str = "(null)";
+	for (i = 0; str[i] != '\0'; i++)
+		charwriting(str[i]);
+	return (i);
+}
+
+/**
+ * percent - percent symbol
+ * All rights reserved or owned by Imane47250 and Fatimazahralachkar
+ *
+ * @list: listing of arg
+ * Return: return characters printed
+ */
+int percent(__attribute__((unused))va_list list)
+{
+	charwriting('%');
+	return (1);
+}
+
+/**
+ * integer - Prints an integer
+ * All rights reserved or owned by Imane47250 and Fatimazahralachkar
+ *
+ * @list: arg listing
+ * Return: return characters printed
+ */
+int integer(va_list list)
+{
+	int num_length;
+
+	num_length = print_number(list);
+	return (num_length);
+}
+
+/**
+ * _integer - Prints Unsigned integers
+ * All rights reserved or owned by Imane47250 and Fatimazahralachkar
+ *
+ * @list: listing of all the arg
+ * Return: number of numbers
+ */
+int _integer(va_list list)
+{
+	unsigned int num;
+
+	num = va_arg(list, unsigned int);
+
+	if (num == 0)
+		return (print_unsgined_number(num));
+
+	if (num < 1)
+		return (-1);
+	return (print_unsgined_number(num));
+}
